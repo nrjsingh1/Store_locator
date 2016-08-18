7ef8d8cca0a59a34e18a6ad2b1f2dcdede64ebaf
Neeraj SIngh <nrjsingh1@yahoo.in>
2016-08-18 09:28:57 -0700

    Add files via upload

---
+++ frame_b.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body background="C:\Users\Administrator\Desktop\image2.png">
+<form action="" method="get">
+<input type="text" name="city" placeholder="Enter the city name"/>
+<br>
+<input type="submit" value="Show the stores"/>
+</form>
+</body>
+</html>
